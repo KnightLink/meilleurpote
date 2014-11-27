21e142012941bf31735d78b3a582b08714b3ddfa
pyrasium <matthiew93@hotmail.fr>
2014-11-27 13:21:36 +0000

    added Board

---
+++ board.py
@@ -0,0 +1,12 @@
+import * from node
+#import * from edge
+class Board:
+    def __init__(self,nb_player = 0):
+        self.nodes = [] ;
+        #self.edges = [] ;
+        self.nb_player = nb_player ;
+        self.speed = 1 ;
+        self.time = 0 ; #millisecondes
+        
+    
+        
